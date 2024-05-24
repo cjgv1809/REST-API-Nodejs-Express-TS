bb7bfe3d2e9bfbced760a3389c082e8ec8ef3f89
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-05-24 11:38:07 -0300

    add orderby option to getProducts handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
 export const getProducts = async (req: Request, res: Response) => {
-  const products = await Product.findAll();
+  const products = await Product.findAll({
+    // order by id
+    order: [["id", "DESC"]],
+  });
 
   res.json({ data: products });
 };
